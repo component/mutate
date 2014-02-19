5d7dade382b26147e91135724a1eb4210d2010ce
Yazhong Liu <yorkiefixer@gmail.com>
2014-02-19 10:36:37 +0800

    add more error information for user-land

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var slice = [].slice;
 
 module.exports = function mutate(nodes) {
   if (arguments.length > 1) nodes = slice.call(arguments);
-  if (!nodes || !nodes.length) throw new Error('Dom Exception 8');
+  if (!nodes || !nodes.length) throw new Error('DOMException 8: NOT_FOUND_ERR');
   if (nodes.length === 1) return toNode(nodes[0]);
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < nodes.length; i++) {
